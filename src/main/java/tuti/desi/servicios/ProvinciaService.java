b9ad7ff4fd407a86ab2cf9abfcecb31e6dba1517
santiespindd <santiagonespindola@gmail.com>
2023-11-20 15:51:23 -0300

    proyecto base

---
+++ src/main/java/tuti/desi/servicios/ProvinciaService.java
@@ -0,0 +1,30 @@
+package tuti.desi.servicios;
+
+
+import java.util.List;
+
+import tuti.desi.entidades.Provincia;
+/**
+ * Clase que permite gestionar la entidad Provincia en el sistema.
+ * @author kuttel
+ * @version 1.0
+ */
+
+public interface ProvinciaService {
+
+	
+
+	/**
+	 * Obtiene la lista completa de Provincias
+	 * @return Todas las Provincias
+	 */
+	@Deprecated
+	List<Provincia> getAll();
+	
+	/**
+	 * Obtiene una Provincia determinada
+	 * @param idCiudad Identificador de la Provincia buscada
+	 * @return Provincia encontrada
+	 */
+	Provincia getById(Long idProv) ;
+}
